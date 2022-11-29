5eb0af6918cdd91f66d5e434a825e108c16313d3
sherin200 <sherinsebastian21>
2022-11-29 14:38:03 +0530

    add service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,19 @@ import { AppComponent } from './app.component';
 import { AddPostComponent } from './add-post/add-post.component';
 import { ViewPostComponent } from './view-post/view-post.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import { HttpClientModule} from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddPostComponent
+    
+  },
+  {
+    path:"view",
+    component:ViewPostComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +28,10 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/view-post/view-post.component.ts
+++ src/app/view-post/view-post.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-view-post',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-post.component.css']
 })
 export class ViewPostComponent {
+
+
+  constructor(private api:ApiService){
+    api.fetchCourses().subscribe(
+      (response)=>
+      {
+        this.posts=response;
+      }
+
+    )
+  }
+
+
+  posts:any=[
+
+  ]
+
 
 }
